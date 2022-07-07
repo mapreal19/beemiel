2aefe70cf512ec312a51cbce2b131ce8205a66a0
Juan Iscar <juan.iscar@exponentiateam.com>
2022-07-07 18:52:21 +0200

    Sendgrid async Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- notification/mailer/mock.go
+++ notification/mailer/mock.go
@@ -2,17 +2,16 @@ package mailer
 
 func mockSender() *emailSender {
 	var send sendInterface
-	send, recorder = mockSend(nil)
+	send, recorder = mockSend()
 	return &emailSender{send}
 
 }
 
 // Inspired by http://tmichel.github.io/2014/10/12/golang-send-test-email/
-func mockSend(errToReturn error) (sendInterface, *Email) {
+func mockSend() (sendInterface, *Email) {
 	r := new(Email)
 
-	return func(email Email) error {
+	return func(email Email) {
 		*r = Email{email.Subject, email.Body, email.PlainBody, email.From, email.Tos, email.Ccs, email.Bccs, email.FromName, email.Attachements}
-		return errToReturn
 	}, r
 }
